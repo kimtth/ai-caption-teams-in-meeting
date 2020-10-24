3bb1b031ce62055870a598250622e8460d251f79
unknown <ultra.taco.0808@gmail.com>
2020-10-24 20:06:54 +0900

    add: screenshot

--- src/components/TabConfig.jsx
+++ src/components/TabConfig.jsx
@@ -19,7 +19,7 @@ function TabConfig(props) {
      * This allows for the addition of query string parameters based on
      * the settings selected by the user.
      */
-    microsoftTeams.settings.setSettings({"contentUrl": "https://d4fcc3cc15d8.ngrok.io/config"});
+    microsoftTeams.settings.setSettings({"contentUrl": "https://54eb9bffc522.ngrok.io/tab"});
 
     /**
      * After verifying that the settings for your tab are correctly
